57f29910ae081cc9644767ba32e97986f1272302
sssun <98_0509@naver.com>
2023-12-21 21:09:47 +0900

    last

---
+++ kotlin-java-study/src/test/kotlin/com/example/kotlinjavastudy/KotlinJavaStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinjavastudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinJavaStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
