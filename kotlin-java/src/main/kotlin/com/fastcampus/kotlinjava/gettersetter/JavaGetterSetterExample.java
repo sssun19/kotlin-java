9570aeeca4913092586772b3f5e56eaa0a597c8f
sssun <98_0509@naver.com>
2023-12-21 21:00:06 +0900

    delete

--- kotlin-java/src/main/kotlin/com/fastcampus/kotlinjava/gettersetter/JavaGetterSetterExample.java
+++ kotlin-java/src/main/kotlin/com/fastcampus/kotlinjava/gettersetter/JavaGetterSetterExample.java
@@ -1,8 +1,15 @@
 package com.fastcampus.kotlinjava.gettersetter;
 
+import java.time.LocalDate;
+
 public class JavaGetterSetterExample {
 
     public static void main(String[] args) {
         Student student = new Student();
+        student.setName("스티브");
+        student.setBirthDate(LocalDate.of(1918, 7, 4));
+
+        System.out.println(student.getName());
+        System.out.println(student.getBirthDate());
     }
 }
