5375c8eb84d71a7f26cb237b8858a33b70412fad
sssun <98_0509@naver.com>
2023-12-26 00:22:31 +0900

    Extension

---
+++ kotlin-java/src/main/kotlin/com/fastcampus/kotlinjava/extensions/ExtensionExample.java
@@ -0,0 +1,10 @@
+package com.fastcampus.kotlinjava.extensions;
+
+public class ExtensionExample {
+
+    public static void main(String[] args) {
+
+        "ABCD".first();
+        "ABCD".addFirst('Z');
+    }
+}
---
+++ kotlin-java/src/main/kotlin/com/fastcampus/kotlinjava/extensions/MyExtensions.kt
@@ -0,0 +1,15 @@
+package com.fastcampus.kotlinjava.extensions
+
+fun String.first(): Char {
+    return this[0]
+}
+
+fun String.addFirst(char: Char) : String {
+    return char + this.substring(0)
+}
+
+fun main() {
+
+    println("ABCD".first())
+    println("ABCD".addFirst('Z'))
+}
