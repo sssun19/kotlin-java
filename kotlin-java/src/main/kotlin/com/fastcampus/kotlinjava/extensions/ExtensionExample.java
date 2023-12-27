242f9e0b2778e7568a94111ad4d4a9cbd7cdead1
sssun <98_0509@naver.com>
2023-12-27 16:58:17 +0900

    MyExtensions

--- kotlin-java/src/main/kotlin/com/fastcampus/kotlinjava/extensions/ExtensionExample.java
+++ kotlin-java/src/main/kotlin/com/fastcampus/kotlinjava/extensions/ExtensionExample.java
@@ -4,7 +4,14 @@ public class ExtensionExample {
 
     public static void main(String[] args) {
 
-        "ABCD".first();
-        "ABCD".addFirst('Z');
+//        "ABCD".first();
+//        "ABCD".addFirst('Z');
+
+        char first = MyExtensionsKt.first("ABCD");
+        String addFirst = MyExtensionsKt.addFirst("ABCD", 'Z');
+
+        System.out.println(first);
+        System.out.println(addFirst);
+
     }
 }
